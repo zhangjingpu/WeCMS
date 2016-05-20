caf473975861e76b7f1383d996d0399ea3e9b728
xiaolinzi <weilaihui@126.com>
2016-05-20 18:00:12 +0800

    add wechat

--- application/Common/Controller/HomebaseController.class.php
+++ application/Common/Controller/HomebaseController.class.php
@@ -13,28 +13,30 @@ class HomebaseController extends AppframeController {
 		parent::_initialize();
 		$site_options=get_site_options();
 		$this->assign($site_options);
-		$ucenter_syn=C("UCENTER_ENABLED");
-		if($ucenter_syn){
-			if(!isset($_SESSION["user"])){
-				if(!empty($_COOKIE['thinkcmf_auth'])  && $_COOKIE['thinkcmf_auth']!="logout"){
-					$thinkcmf_auth=sp_authcode($_COOKIE['thinkcmf_auth'],"DECODE");
-					$thinkcmf_auth=explode("\t", $thinkcmf_auth);
-					$auth_username=$thinkcmf_auth[1];
-					$users_model=M('Users');
-					$where['user_login']=$auth_username;
-					$user=$users_model->where($where)->find();
-					if(!empty($user)){
-						$is_login=true;
-						$_SESSION["user"]=$user;
-					}
-				}
-			}else{
-			}
-		}
-		
-		if(sp_is_user_login()){
-			$this->assign("user",sp_get_current_user());
-		}
+//		$ucenter_syn=C("UCENTER_ENABLED");
+//		if($ucenter_syn){
+//			if(!isset($_SESSION["user"])){
+//				if(!empty($_COOKIE['thinkcmf_auth'])  && $_COOKIE['thinkcmf_auth']!="logout"){
+//					$thinkcmf_auth=sp_authcode($_COOKIE['thinkcmf_auth'],"DECODE");
+//					$thinkcmf_auth=explode("\t", $thinkcmf_auth);
+//					$auth_username=$thinkcmf_auth[1];
+//					$users_model=M('Users');
+//					$where['user_login']=$auth_username;
+//					$user=$users_model->where($where)->find();
+//					if(!empty($user)){
+//						$is_login=true;
+//						$_SESSION["user"]=$user;
+//					}
+//				}
+//			}else{
+//			}
+//		}
+//
+//		if(sp_is_user_login()){
+//			$this->assign("user",sp_get_current_user());
+//		}
+
+		$openid = $this->getOpenId();
 		
 	}
 	
